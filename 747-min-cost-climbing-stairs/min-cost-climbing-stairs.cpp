e2b83ef7e96ad987cc04c67979702555e01881ce
Piyush singh <piyushsinghiiitv@gmail.com>
2026-08-24 22:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (75.43%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-
-     int dp[1002];
-    int f(vector<int>&cost,int i){
-        int n=cost.size();
-        if(i==n) return 0;
-        if(i>n) return 1e9;
-        if(dp[i]!=-1) return dp[i];
-        int ans1=cost[i]+f(cost,i+1);
-        int ans2=cost[i]+f(cost,i+2);
-        return  dp[i]=min(ans1,ans2);
-    }
+   
+    //  int dp[1002];
+    // int f(vector<int>&cost,int i){
+    //      dp[n]=0;
+    //      dp[n+1]=1e9;    
+    //     // int n=cost.size();
+    //     // if(i==n) return 0;
+    //     // if(i>n) return 1e9;
+    //     // if(dp[i]!=-1) return dp[i];
+    //     // int ans1=cost[i]+f(cost,i+1);
+    //     // int ans2=cost[i]+f(cost,i+2);
+    //     // return  dp[i]=min(ans1,ans2);
+    //     for(int i=n-1;i>=0;i--){
+    //       dp[i]=cost[i]+f(dp[i+1],dp[i+2]);
+    //     } 
+    //     return min(dp[0],dp[1]);
+    // }    
     int minCostClimbingStairs(vector<int>& cost) {
-        memset(dp,-1,sizeof(dp));
-       int finalans1=f(cost,0);
-       int finalans2=f(cost,1);
-       
-       return min(finalans1,finalans2);  
-
+        // memset(dp,-1,sizeof(dp));
+        int n=cost.size();
+    //    int finalans1=f(cost,0);
+    //    int finalans2=f(cost,1);
+      
+    //    return min(finalans1,finalans2);
+      vector<int>dp(n+2,0);
+    //  dp[n]=0;
+         dp[n+1]=1e9;
+       for(int i=n-1;i>=0;i--){
+          dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
+        }  
+        return min(dp[0],dp[1]);      
+        
      }
 };
