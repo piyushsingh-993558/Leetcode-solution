22f781303f4add12649b462059817f8cf3cd792f
Piyush singh <piyushsinghiiitv@gmail.com>
2026-08-30 04:19:21 +0530

    Time: 3 ms (68.09%) | Memory: 13.3 MB (75.71%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,54 +1,32 @@
 class Solution {
 public:
-    vector<string> finalans;
+    vector<string> ans;
 
-    bool isValid(string& curr) {
-        int ptr = 0;
+    void f(int n, string& curr, int open, int close) {
 
-        for (int i = 0; i < curr.size(); i++) {
-
-            if (curr[i] == '(') {
-                ptr++;
-            }
-            else {
-                ptr--;
-
-                if (ptr < 0)
-                    return false;
-            }
-        }
-
-        return ptr == 0;
-    }
-
-    void f(int n, string& curr) {
-
-        // Base case
+      
         if (curr.size() == 2 * n) {
-
-            if (isValid(curr)) {
-                finalans.push_back(curr);
-            }
-
+            ans.push_back(curr);
             return;
         }
 
-      
-        curr.push_back('(');
-        f(n, curr);
-        curr.pop_back();
+        
+        if (open < n) {
+            curr.push_back('(');
+            f(n, curr, open + 1, close);
+            curr.pop_back();
+        }
 
-       
-        curr.push_back(')');
-        f(n, curr);
-        curr.pop_back();
+        if (close < open) {
+            curr.push_back(')');
+            f(n, curr, open, close + 1);
+            curr.pop_back();
+        }
     }
 
     vector<string> generateParenthesis(int n) {
         string curr = "";
-
-        f(n, curr);
-
-        return finalans;
+        f(n, curr, 0, 0);
+        return ans;
     }
 };
