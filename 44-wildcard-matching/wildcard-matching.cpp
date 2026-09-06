ab06dffce15073472ee37d7efd7465da97cc597d
Piyush singh <piyushsinghiiitv@gmail.com>
2026-09-07 02:05:23 +0530

    Time: 64 ms (27.50%) | Memory: 54.8 MB (13.66%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
-bool f(string& s,string& p,int i,int j,vector<vector<int>>&dp){
+bool f(string& s,string& p,vector<vector<int>>&dp){
    int n1=s.size(); 
    int n2=p.size();
-   if(i==n1&&j==n2) return true;
-   if(i<n1&&j==n2) return false; 
-   if(i>=n1&&j<n2){
-    for(int k=j;k<n2;k++){
-        if(p[k]!='*') return false;
-    }
-     return true;
+   dp[n1][n2]=true;
+     
+ 
+   for(int i=0;i<n1;i++){
+     dp[i][n2]=false;
    } 
-   if(dp[i][j]!=-1) return dp[i][j];
-   if(s[i]==p[j]||p[j]=='?'){
-    return dp[i][j]= f(s,p,i+1,j+1,dp);
-   }else if(p[j]=='*'){
-    return dp[i][j]=f(s,p,i+1,j,dp)||f(s,p,i,j+1,dp);
-   }else{
-    return dp[i][j]=false;
-   }
+
+   for(int j=0;j<n2;j++){
+    bool flag=true;
+    for(int k=j;k<n2;k++){
+        if(p[k]!='*'){
+         flag=false;
+        }
     }
-    bool isMatch(string s, string p) {
-        int n1=s.size();
-        int n2=p.size();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-   return f(s,p,0,0,dp);
+     dp[n1][j]=flag;
+   }
+    for(int i=n1-1;i>=0;i--){
+        for(int j=n2-1;j>=0;j--){
+         if(s[i]==p[j]||p[j]=='?'){
+     dp[i][j]= dp[i+1][j+1];
+   }else if(p[j]=='*'){ 
+     dp[i][j]=dp[i+1][j]||dp[i][j+1];
+   }else{
+     dp[i][j]=false;
+   }
+        }
+    }
+     return dp[0][0];
     
+    } 
+    bool isMatch(string s, string p) {
+        int n1=s.size(); 
+        int n2=p.size();
+    vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
+   return f(s,p,dp);
+     
     }
 };
